577705bb2f25a49f052a0f466e876f759881a06c
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2020-06-06 18:24:00 -0300

    criando o auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,86 @@
-import { UsuarioListaComponent } from "./components/usuario-lista/usuario-lista.component";
-import { UsuarioDetalhesComponent } from "./components/usuario-detalhes/usuario-detalhes.component";
-import { UsuarioNovoComponent } from "./components/usuario-novo/usuario-novo.component";
-import { DisciplinaNovoComponent } from "./components/disciplina-novo/disciplina-novo.component";
-import { DashboardComponent } from "./components/dashboard/dashboard.component";
-import { LoginComponent } from "./components/login/login.component";
-import { PlanoNovoComponent } from "./components/plano-novo/plano-novo.component";
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { PlanoDetalhesComponent } from "./components/plano-detalhes/plano-detalhes.component";
-import { PlanoListComponent } from "./components/plano-list/plano-list.component";
-import { DisciplinaDetalhesComponent } from "./components/disciplina-detalhes/disciplina-detalhes.component";
-import { DisciplinaListaComponent } from "./components/disciplina-lista/disciplina-lista.component";
+import { AuthGuard } from './auth.guard';
+import { UsuarioListaComponent } from './components/usuario-lista/usuario-lista.component';
+import { UsuarioDetalhesComponent } from './components/usuario-detalhes/usuario-detalhes.component';
+import { UsuarioNovoComponent } from './components/usuario-novo/usuario-novo.component';
+import { DisciplinaNovoComponent } from './components/disciplina-novo/disciplina-novo.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { PlanoNovoComponent } from './components/plano-novo/plano-novo.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PlanoDetalhesComponent } from './components/plano-detalhes/plano-detalhes.component';
+import { PlanoListComponent } from './components/plano-list/plano-list.component';
+import { DisciplinaDetalhesComponent } from './components/disciplina-detalhes/disciplina-detalhes.component';
+import { DisciplinaListaComponent } from './components/disciplina-lista/disciplina-lista.component';
 
 const routes: Routes = [
-  { path: "", pathMatch: "full", redirectTo: "login" },
-  { path: "login", component: LoginComponent },
-  { path: "dashboard", component: DashboardComponent },
-  { path: "novo-plano", component: PlanoNovoComponent },
-  { path: "novo-plano/:id", component: PlanoNovoComponent },
-  { path: "detalhes-plano/:id", component: PlanoDetalhesComponent },
-  { path: "lista-planos", component: PlanoListComponent },
-  { path: "nova-disciplina", component: DisciplinaNovoComponent },
-  { path: "nova-disciplina/:id", component: DisciplinaNovoComponent },
-  { path: "detalhes-disciplina/:id", component: DisciplinaDetalhesComponent },
-  { path: "lista-disciplinas", component: DisciplinaListaComponent },
-  { path: "novo-usuario", component: UsuarioNovoComponent },
-  { path: "novo-usuario/:id", component: UsuarioNovoComponent },
-  { path: "detalhes-usuario/:id", component: UsuarioDetalhesComponent },
-  { path: "lista-usuarios", component: UsuarioListaComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'novo-plano',
+    component: PlanoNovoComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'novo-plano/:id',
+    component: PlanoNovoComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'detalhes-plano/:id',
+    component: PlanoDetalhesComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'lista-planos',
+    component: PlanoListComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'nova-disciplina',
+    component: DisciplinaNovoComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'nova-disciplina/:id',
+    component: DisciplinaNovoComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'detalhes-disciplina/:id',
+    component: DisciplinaDetalhesComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'lista-disciplinas',
+    component: DisciplinaListaComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'novo-usuario',
+    component: UsuarioNovoComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'novo-usuario/:id',
+    component: UsuarioNovoComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'detalhes-usuario/:id',
+    component: UsuarioDetalhesComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'lista-usuarios',
+    component: UsuarioListaComponent,
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,30 @@
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (sessionStorage.getItem('eduplan_token') === null) {
+      this.router.navigate(['login']);
+      return false;
+    }
+    return true;
+  }
+}
